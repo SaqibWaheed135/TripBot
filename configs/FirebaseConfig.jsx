85b3fc34d9bfd7b5a3789a3d5ce3a8abb0cc7913
Saqib135 <xaqibwaheed333@gmail.com>
2025-09-13 17:30:04 +0500

    latest commit

--- configs/FirebaseConfig.jsx
+++ configs/FirebaseConfig.jsx
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore"; // 👈 Add this
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDvxxB8Ds53nMzSIBJFCURSkNrs9LznwCs",
   authDomain: "tripbot-2eded.firebaseapp.com",
   projectId: "tripbot-2eded",
-  storageBucket: "tripbot-2eded.firebasestorage.app",
+  storageBucket: "tripbot-2eded.appspot.com",  // 👈 should be .appspot.com not .firebasestorage.app
   messagingSenderId: "911525943345",
   appId: "1:911525943345:web:f170372c023a86fae3dd10",
   measurementId: "G-32H2M6HH0J"
@@ -20,4 +18,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-
+export const db = getFirestore(app); // 👈 Add this
